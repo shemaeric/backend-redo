5906be568330973819af16e2b01e2720f0b790d6
shemaeric <shemaeric01@gmail.com>
2020-01-27 14:44:54 +0200

    add django mongodb functionalities and populate scrapped data

---
+++ app/book/models.py
@@ -0,0 +1,20 @@
+# from django.db import models
+from __future__ import unicode_literals
+from djongo import models
+
+class Book(models.Model):
+
+    uuid = models.CharField(max_length = 40, null = False)
+    title = models.CharField(max_length = 40, null = False)
+    main_image = models.CharField(max_length = 40, null = False)
+    price = models.IntegerField(null = False)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        self.uuid
+
+
+
+
+
--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app.book.book'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'djongo',
+        'NAME': os.environ.get('DATABASE', 'bookstore'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('PORT', 27017),
     }
 }
 
