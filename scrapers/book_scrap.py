9dabb7612442bf2cb357171ed5297b3e89401768
shemaeric <shemaeric01@gmail.com>
2020-01-27 14:44:42 +0200

    add django mongodb functionalities and populate scrapped data

--- scrapers/book_scrap.py
+++ scrapers/book_scrap.py
@@ -11,9 +11,21 @@ from pprint import pprint
 from time import sleep
 import uuid
 import time
+from pymongo import MongoClient
+import uuid
 
 env_path = Path('../') / '.env'
 load_dotenv(dotenv_path=env_path)
+
+# create connection with mongoDB
+
+DATABASE = os.getenv('DATABASE')
+
+client = MongoClient()
+
+db = client[DATABASE]
+
+table = db['book_book']
 
 
 def get_urls(url):
@@ -41,9 +53,33 @@ def scrape_book(url, base_url):
 
     try:
         links = get_urls(url)
+        data = []
         for link in links:
             book_request = requests.get(base_url + link)
-            print(book_request)
+            soup = BeautifulSoup(book_request.content, 'html5lib')
+            book_details_section = soup.find_all('section', itemtype = 'http://schema.org/Product')
+            data_dict = dict()
+
+            for detail in book_details_section:
+                id_section = uuid.uuid4().hex
+                data_dict['id'] = id_section
+                data_dict['uuid'] = id_section
+                data_dict['title'] = detail.find('span', itemprop = 'name').text
+                data_dict['main_image'] = detail.find('img', id="product-cover-image")['src']
+                price = detail.find('dl', class_="price")
+                data_dict['price'] = int(price.find('span', class_ = None).text)
+                description = detail.find('div', itemprop = 'description')
+                if description is None:
+                    data_dict['description'] = ''
+                else:
+                    text = description.find('p', class_ = None)
+                    if text is None:
+                       data_dict['description'] = ''
+                    else:
+                         data_dict['description'] = description.find('p', class_ = None).text
+                    
+            # Insert the data in database.
+            table.insert(data_dict)
     
     except Exception as e:
         print('error', e)
